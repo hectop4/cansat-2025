7b7edd77f0c36d969702d51925111103bf626cc9
Hector Puentes <hectorjosepuentesruiz@gmail.com>
2024-12-26 06:39:12 -0500

    Fixing size of window in the HMI

--- test.py
+++ test.py
@@ -1,1 +1,15 @@
-print('Hello world')
+#start hmi.ui file with pyqt5
+
+import sys
+from PyQt5.QtWidgets import QApplication, QMainWindow
+from PyQt5 import QtCore
+from PyQt5 import uic
+
+#remove the window title bar
+
+app = QApplication(sys.argv)
+window = QMainWindow()
+window.setWindowFlags(QtCore.Qt.FramelessWindowHint)
+uic.loadUi("hmi.ui", window)
+window.show()
+sys.exit(app.exec_())
