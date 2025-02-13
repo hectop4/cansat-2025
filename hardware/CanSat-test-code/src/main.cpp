93f882384eb7c6ec48c3d886f153d24b75bf11d1
Hector Puentes <hectorjosepuentesruiz@gmail.com>
2025-02-13 10:05:16 -0500

    adding?

--- hardware/CanSat-test-code/src/main.cpp
+++ hardware/CanSat-test-code/src/main.cpp
@@ -8,51 +8,112 @@
 #include <ESP32Servo.h>
 #include <MQ135.h>
 #include <MQUnifiedsensor.h>
+#include <string.h>
 #define esp32_sample_rate 4096
 
-//
+// I2C Pins
 #define SDA 2
 #define SCL 42
+// Servomotor
 #define Servomotor 41
+// GPS Pins
 #define GPSRX 47
 #define GPSTX 21
+#define GPS_BAUD 9600
+// Lora RYLR998
 #define LORARX 36
 #define LORATX 37
-#define GPS_BAUD 9600
 
-//
-// #define airquality 1
-//
-Adafruit_MPU6050 mpu;
-Adafruit_BMP085 bmp;
-
-TinyGPSPlus gps;
-
-HardwareSerial gpsSerial(2);
-
-Servo myservo;
-
-// Definitions
+// Air Quality Sensor Definitions
 #define placa "ESP-32"
 #define Voltage_Resolution 3.3
-#define pin 1                  // Analog input 0 of your arduino
-#define type "MQ-135"          // MQ135
-#define ADC_Bit_Resolution 12  // For arduino UNO/MEGA/NANO
+#define pin 1
+#define type "MQ-135"
+#define ADC_Bit_Resolution 12
 #define RatioMQ135CleanAir 3.6 // RS / R0 = 3.6 ppm
-// #define calibration_button 13 //Pin to calibrate your sensor
 
-// Declare Sensor
+// Initialize the sensors
+Adafruit_MPU6050 mpu;
+Adafruit_BMP085 bmp;
+TinyGPSPlus gps;
+HardwareSerial gpsSerial(2);
+Servo myservo;
 MQUnifiedsensor MQ135(placa, Voltage_Resolution, ADC_Bit_Resolution, pin, type);
+SoftwareSerial ReyaxLoRa(LORARX, LORATX); //--> RX, TX
+
+// Variables
+float ppm = 0;
+float pressure = 0;
+float temperature = 0;
+float altitude = 0;
+float latitude = 0;
+float longitude = 0;
+float height_limit = 200;
+float accx = 0;
+float accy = 0;
+float accz = 0;
+float gyx = 0;
+float gyy = 0;
+float gyz = 0;
+
+String message = "";
+int msg_len = 0;
 
 void setup()
 {
   // Init the serial port communication - to debug the library
-  Serial.begin(115200); // Init serial port
+  Serial.begin(115200);
+  // Init the LoRa module
+  ReyaxLoRa.begin(115200);
+  Serial.println("LoRa: OK");
+  // Init Servomotor
+  myservo.attach(Servomotor);
+  myservo.write(0);
+  Serial.println("Servomotor: OK");
 
+  // Init GPS
+  // Start Serial 2 with the defined RX and TX pins and a baud rate of 9600
+  gpsSerial.begin(GPS_BAUD, SERIAL_8N1, GPSRX, GPSTX);
+  Serial.println("GPS: OK");
+
+  // Init BMP and MPU
+  Wire.begin(SDA, SCL);
+  if (!mpu.begin())
+  {
+    Serial.println("MPU: FAILED");
+    while (1)
+    {
+      delay(10);
+    }
+  }
+  Serial.println("MPU6050: OK");
+  delay(500);
+  if (!bmp.begin())
+  {
+    Serial.println("BMP: FAILED");
+    while (1)
+    {
+      delay(10);
+    }
+  }
+
+  Serial.println("BMP180: OK");
+  delay(500);
+
+  mpu.setAccelerometerRange(MPU6050_RANGE_8_G);
+  Serial.println("Accelerometer range set to: +-8G");
+
+  mpu.setGyroRange(MPU6050_RANGE_500_DEG);
+  Serial.println("Gyro range set to: +- 500 deg/s");
+
+  mpu.setFilterBandwidth(MPU6050_BAND_21_HZ);
+  Serial.println("Filter bandwidth set to: 21 Hz");
+
+  // Init MQ135
   // Set math model to calculate the PPM concentration and the value of constants
   MQ135.setRegressionMethod(1); //_PPM =  a*ratio^b
   MQ135.setA(110.47);
-  MQ135.setB(-2.862); // Configure the equation to to calculate NH4 concentration
+  MQ135.setB(-2.862); // Configure the equation to to calculate CO2 concentration
 
   /*
     Exponential regression:
@@ -69,22 +130,12 @@ void setup()
   // Remarks: Configure the pin of arduino as input.
   /************************************************************************************/
   MQ135.init();
-
-  // If the RL value is different from 10K please assign your RL value with the following method:
-  // MQ135.setRL(10);
-
   /*****************************  MQ CAlibration ********************************************/
-  // Explanation:
-  // In this routine the sensor will measure the resistance of the sensor supposedly before being pre-heated
-  // and on clean air (Calibration conditions), setting up R0 value.
-  // We recomend executing this routine only on setup in laboratory conditions.
-  // This routine does not need to be executed on each restart, you can load your R0 value from eeprom.
-  // Acknowledgements: https://jayconsystems.com/blog/understanding-a-gas-sensor
-  Serial.print("Calibrating please wait.");
+  Serial.println("Calibrating MQ135 please wait.");
   float calcR0 = 0;
   for (int i = 1; i <= 10; i++)
   {
-    MQ135.update(); // Update data, the arduino will read the voltage from the analog pin
+    MQ135.update();
     calcR0 += MQ135.calibrate(RatioMQ135CleanAir);
     Serial.print(".");
   }
@@ -93,56 +144,72 @@ void setup()
 
   if (isinf(calcR0))
   {
-    Serial.println("Warning: Conection issue, R0 is infinite (Open circuit detected) please check your wiring and supply");
+    Serial.println("MQ135 Calibration: FAILED (Open-Circuit)");
     while (1)
       ;
   }
   if (calcR0 == 0)
   {
-    Serial.println("Warning: Conection issue found, R0 is zero (Analog pin shorts to ground) please check your wiring and supply");
+    Serial.println("MQ135 Calibration: FAILED (Short-Circuit)");
     while (1)
       ;
   }
   /*****************************  MQ CAlibration ********************************************/
   MQ135.serialDebug(true);
+  Serial.println("MQ135: OK");
 }
+
 void loop()
 {
-  MQ135.update();                              // Update data, the arduino will read the voltage from the analog pin
-  MQ135.readSensor();                          // Sensor will read PPM concentration using the model, a and b values set previously or from the setup
-  Serial.println(MQ135.readSensor() * 100, 1); // Will print the table on the serial port
-  delay(500);                                  // Sampling frequency
+  unsigned long start = millis();
+  sensors_event_t a, g, temp;
+  mpu.getEvent(&a, &g, &temp);
+
+  MQ135.update();
+  ppm = MQ135.readSensor() * 100;
+  // Serial.println(ppm);
+  // delay(500);
+  temperature = bmp.readTemperature();
+  pressure = bmp.readPressure();
+  altitude = bmp.readAltitude();
+
+  accx = a.acceleration.x;
+  accy = a.acceleration.y;
+  accz = a.acceleration.z;
+  gyx = g.gyro.x;
+  gyy = g.gyro.y;
+  gyz = g.gyro.z;
+
+  message = "T:" + String(temperature) + ",P:" + String(pressure) + ",H:" + String(altitude) + ",PPM:" + String(ppm) + ",AX:" + String(accx) + ",AY:" + String(accy) + ",AZ:" + String(accz) + ",GX:" + String(gyx) + ",GY:" + String(gyy) + ",GZ:" + String(gyz) + ",LAT:" + String(latitude) + ",LON:" + String(longitude);
+  msg_len = message.length() - 2;
+  Serial.println(message);
+
+  String str_Send = "AT+SEND=1," + String(msg_len) + "," + message; //--> "AT+SEND=Recipient/Destination Address, Payload Length, Data/Message".
+  ReyaxLoRa.print(str_Send);
+  Serial.print("Send to Receiver : ");
+  Serial.print(str_Send);
+  Serial.flush();
+  if (ReyaxLoRa.available())
+    Serial.println(ReyaxLoRa.readString());
+
+  while (millis() - start < 1000)
+  {
+    while (gpsSerial.available() > 0)
+    {
+      // char gpsData = gpsSerial.read();
+      // Serial.print(gpsData);
+      gps.encode(gpsSerial.read());
+    }
+    if (gps.location.isUpdated())
+    {
+      // Serial.println(gps.location.lat(), 6);
+      latitude = gps.location.lat();
+      // Serial.println(gps.location.lng(), 6);
+      longitude = gps.location.lng();
+      // Serial.println("");
+    }
+  }
 }
-
-//! Code for MQ135 in PPM V1
-
-// void setup()
-// {
-//   Serial.begin(115200);
-//   int avg = 0;
-//   for (int i = 0; i < 10; i++)
-//   {
-//     avg += analogRead(airquality);
-//     delay(2000);
-//   }
-//   avg = avg / 10;
-//   MQ135_init(esp32_sample_rate, avg);
-// }
-
-// void loop()
-// {
-
-//   int adc = analogRead(airquality);
-//   int CO2 = read_CO2(esp32_sample_rate, adc);
-//   int NH4 = read_NH4(esp32_sample_rate, adc);
-
-//   Serial.println(adc);
-//   Serial.print("CO2_PPM:  ");
-//   Serial.println(CO2);
-//   Serial.print("NH4_PPM:  ");
-//   Serial.println(NH4);
-//   delay(2000);
-// }
 
 //! Code for Servomotor
 
